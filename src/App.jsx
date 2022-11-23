813a911ca0cfbfde4e6e3393103c34f3e99668d9
Matt Howell <matt.howell@covenanteyes.com>
2022-11-23 16:16:10 -0600

    grab questions and answers from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+// libraries
+import axios from 'axios'
+
+// custom components
 import FlashcardList from './components/FlashcardList'
 
 function App() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  const [flashcards, setFlashcards] = useState([])
+
+  useEffect(() => {
+    axios.get('https://opentdb.com/api.php?amount=10')
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer)
+        const options = [
+          ...questionItem.incorrect_answers.map(a => decodeString(a)), 
+          answer
+        ]
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: questionItem.correct_answer,
+          options: options.sort(() => Math.random() - .5)
+        }
+      }))
+      console.log(res.data)
+      
+    })
+  }, [])
+
+  const decodeString = (str) => {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
 
   return (
     <div className="App">
@@ -10,30 +42,5 @@ function App() {
     </div>
   )
 }
-
-const SAMPLE_FLASHCARDS = [
- {
-  id: 1,
-  question: 'What is 2 + 3?',
-  answer: '4',
-  options: [
-    '2',
-    '3',
-    '4',
-    '5'
-  ]
- },
- {
-  id: 1,
-  question: 'Question 2?',
-  answer: 'Answer',
-  options: [
-    'Answer',
-    'Answer 2',
-    'Answer 3',
-    'Answer 4'
-  ]
- }
-]
 
 export default App
